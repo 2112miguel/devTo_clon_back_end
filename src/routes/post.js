b549a35dcb3c4bc9b735c7139494a228b231b2d1
2112miguel <miguel.moreno.angel@gmail.com>
2022-04-26 14:17:56 -0500

    Fix Bug

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const post = require("../usescases/post");
+const post = require("../usecases/post");
 
 router.get("/", async (req, res, next) => {
   try {
