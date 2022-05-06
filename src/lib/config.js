5f5a2c7815f07dcef309491d724d49f16e33311f
2112miguel <miguel.moreno.angel@gmail.com>
2022-05-06 13:14:20 -0500

    Modify Port

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,16 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const config={
-    app: {
-        port: process.env.APP_PORT || 80,
-        jwt: process.env.JWT_SECRET,
-    },
-    db: {
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        baseCollection: process.env.DB_BASE_COLLECTION,
-        
-    }
-}
+const config = {
+  app: {
+    port: process.env.APP_PORT || 3000,
+    jwt: process.env.JWT_SECRET,
+  },
+  db: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    baseCollection: process.env.DB_BASE_COLLECTION,
+  },
+};
 module.exports = config;
