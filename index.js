3348bbd826bdf0afe988bc7932a6fab67aa6fbdb
2112miguel <miguel.moreno.angel@gmail.com>
2022-05-06 12:11:24 -0500

    Modify Port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 3000;
 const db = require("./src/lib/db");
 const config = require("./src/lib/config");
 const apiRouter = require("./src/routes");
